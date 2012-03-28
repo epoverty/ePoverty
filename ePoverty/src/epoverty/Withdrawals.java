4ed5c5c208e5df056270fc7db55ec65d1c3e8f39
Trevor Hackett <trevorhackett@gmail.com>
2012-03-28 17:39:11 -0600

    changed package for withdrawals

--- ePoverty/src/epoverty/Withdrawals.java
+++ ePoverty/src/epoverty/Withdrawals.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package backendtest;
+package epoverty;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
